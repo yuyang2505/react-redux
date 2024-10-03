d50d28fd8ce0774da937f9a30fbf1ce7882e6bde
yuyang <ongyuyang2505@gmail.com>
2024-10-03 02:24:49 -0400

    completed

---
+++ src/Components/SuperCoin.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react'
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+const SuperCoin = () => {
+    const[superCoins, setSuperCoins] = useState(0);
+
+    const cartItems = useSelector(state => state.cart.cartItems);
+
+    const totalAmount = cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+
+    useEffect(() => {
+    if (totalAmount >= 100 && totalAmount < 200) {
+        setSuperCoins(10);
+    } else if (totalAmount >= 200 && totalAmount < 300) {
+        setSuperCoins(20);
+    } else if (totalAmount >= 300) {
+        setSuperCoins(30);
+    } else {
+        setSuperCoins(0);
+    }
+    }, [totalAmount]);
+
+    return(
+        <>
+        <div className="super-coins" style={{textAlign:'center'}}>
+            <h2 className="super-coins-title">Super Coins</h2>
+            <p className="super-coins-info">You will earn {superCoins} super coins with this purchase.</p>
+        </div>
+        </>
+    )
+}
+
+export default SuperCoin
